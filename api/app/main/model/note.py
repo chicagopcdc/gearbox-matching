fad0a77d0005bbe91ae7d5872c714479b3037a81
tohara4 <tom4@uchicago.edu>
2021-01-21 14:17:06 -0600

    note String, not limitted with String(45). ontology_code_id/input_type_id added as columns to criterion (missing, already set in db/pedal.sql as firegn keys with constraints). criterions (children class) relationship mapped to parent ontology_code.

--- api/app/main/model/note.py
+++ api/app/main/model/note.py
@@ -7,6 +7,6 @@ from . import Base
 class Note(Base):
     __tablename__ = 'note'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    value = Column(String(45), nullable=True)
+    value = Column(String, nullable=True)
 
     eligibility_criterias = relationship("EligibilityCriteriaHasNote", back_populates="note")
--- api/app/main/model/ontology_code.py
+++ api/app/main/model/ontology_code.py
@@ -6,9 +6,12 @@ from . import Base
 
 class OntologyCode(Base):
     __tablename__ = 'ontology_code'
+    
     id = Column(Integer, primary_key=True, autoincrement=True)
     ontology_url = Column(String(45), nullable=True)
     name = Column(String(45), nullable=True)
     code = Column(String(45), nullable=True)
     value = Column(String(45), nullable=True)
     version = Column(String(45), nullable=True)
+
+    criterions = relationship("Criterion", back_populates="ontology_code")
