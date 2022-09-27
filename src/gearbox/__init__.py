af1e44cb805ebb4875d0250fd1d1ff8ce11ac128
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2022-09-27 16:23:50 -0500

    Cleanup deleted files.

--- src/gearbox/__init__.py
+++
@@ -1,5 +0,0 @@
-import cdislogging
-from . import config
-
-
-logger = cdislogging.get_logger(__name__, log_level="debug" if config.DEBUG else "info")
--- src/gearbox/schemas/site_schema_test.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class SiteSchemaTest(BaseModel):
-    id: int
-    name: str
-    code: str
-    create_date: datetime
-    active: bool
