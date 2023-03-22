3181a350f66b21390814d4627a110a5a761e4706
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2023-03-21 21:39:08 -0400

    Added test for duplicate data in input.

--- tests/test_el_criteria_has_criterion.py
+++ tests/test_el_criteria_has_criterion.py
@@ -71,6 +71,35 @@ def test_create_el_criteria_has_criterion(setup_database, client, data):
             "value_id": 22 
                 },
                 {
+            "criterion_id": 1,
+            "eligibility_criteria_id": 1,
+            "active": True,
+            "value_id": 22 
+                },
+            ]
+        }
+    ]
+)
+@pytest.mark.asyncio
+def test_create_duplicate_el_criteria_has_criterion(setup_database, client, data):
+    """
+    Test create el_criteria_has_criterion
+    """
+    fake_jwt = "1.2.3"
+    resp = client.post("/el-criteria-has-criterion", json=data, headers={"Authorization": f"bearer {fake_jwt}"})
+    assert str(resp.status_code).startswith("409")
+
+@pytest.mark.parametrize(
+    "data", [ 
+        { "echcs":
+            [
+                {
+            "criterion_id": 1,
+            "eligibility_criteria_id": 1,
+            "active": True,
+            "value_id": 22 
+                },
+                {
             "criterion_id": 8,
             "eligibility_criteria_id": 2,
             "active": True,
@@ -81,7 +110,7 @@ def test_create_el_criteria_has_criterion(setup_database, client, data):
     ]
 )
 @pytest.mark.asyncio
-def test_create_duplicate_el_criteria_has_criterion(setup_database, client, data):
+def test_create_duplicate_db_el_criteria_has_criterion(setup_database, client, data):
     """
     Test create el_criteria_has_criterion
     """
