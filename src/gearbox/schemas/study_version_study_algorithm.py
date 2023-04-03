299195f2d16a1425ce63dc86e6cf7ac00d9dfb7b
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2023-04-03 13:50:58 -0500

    Removed comment and line 15.

--- src/gearbox/schemas/study_version_study_algorithm.py
+++ src/gearbox/schemas/study_version_study_algorithm.py
@@ -8,8 +8,5 @@ class StudyVersionStudyAlgorithmCreate(StudyVersionStudyAlgorithmBase):
     eligibility_criteria_info_id: int
 
 class StudyVersionStudyAlgorithm(StudyVersionStudyAlgorithmBase):
-    # study_algorithm_engine: StudyAlgorithmEngineCreate
     eligibility_criteria_info_id: int
     study_algorithm_engine: StudyAlgorithmEngine
-
-StudyVersionStudyAlgorithm, StudyVersionStudyAlgorithmCreate
