f1bf61abac13db4f1ac80c762f0499c7fc1c3d12
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2023-06-26 14:21:32 -0500

    Modified match conditions query to use EligibilityCriteriaInfoStatus type.

--- src/gearbox/crud/match_conditions.py
+++ src/gearbox/crud/match_conditions.py
@@ -4,8 +4,8 @@ from sqlalchemy import select, exc
 from typing import Generic, List, Optional, Type, TypeVar, Union
 
 from sqlalchemy.orm import Session 
-
 from gearbox.models import EligibilityCriteriaInfo
+from gearbox.util.types import EligibilityCriteriaInfoStatus
 
 from cdislogging import get_logger
 logger = get_logger(__name__)
@@ -13,7 +13,7 @@ logger = get_logger(__name__)
 
 async def get_study_algorithm_engines(session: Session)->List[EligibilityCriteriaInfo]:
 
-    stmt = select(EligibilityCriteriaInfo).filter(EligibilityCriteriaInfo.active == True)
+    stmt = select(EligibilityCriteriaInfo).filter(EligibilityCriteriaInfo.status == EligibilityCriteriaInfoStatus.ACTIVE.value)
     result = await session.execute(stmt)
     ae = result.unique().scalars().all()
 
