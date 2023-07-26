20ee34cf368b5f5c1530c89e816084ea06825215
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2023-07-26 08:57:31 -0500

    Added site getter method to study pydantic schema for m2m table.

--- src/gearbox/schemas/study.py
+++ src/gearbox/schemas/study.py
@@ -1,10 +1,32 @@
 #from urllib.request import HTTPDefaultErrorHandler
 from pydantic import BaseModel #, HttpUrl
 from datetime import datetime
-from typing import Sequence, List, Optional
+from typing import Sequence, List, Optional, Any
 from gearbox.schemas import SiteCreate #, StudyLinkCreate
 from .study_link import StudyLinkCreate, StudyLink
-from .site import Site, SiteStudy
+from .site_has_study import SiteHasStudy
+from .site import Site
+
+from pydantic.utils import GetterDict
+
+class StudySiteGetter(GetterDict):
+    # map and reformat study fields
+    def get(self, key: str, default: Any = None) -> Any:
+        if key in ('id','name','code','create_date','active'):
+            return getattr(self._obj.site, key)
+        else:
+            return super(StudySiteGetter, self).get(key, default)
+
+class StudySite(BaseModel):
+    id: int
+    name: Optional[str]
+    code: Optional[str]
+    create_date: Optional[datetime]
+    active: Optional[bool]
+
+    class Config:
+        orm_mode = True
+        getter_dict = StudySiteGetter
 
 class StudyBase(BaseModel):
     name: Optional[str]
@@ -18,8 +40,9 @@ class StudyBase(BaseModel):
 
 class Study(StudyBase):
     id: int
-    sites: List[SiteStudy]
     links: List[StudyLink]
+    sites: List[StudySite]
+
 
 class StudyCreate(StudyBase):
     sites: Optional[List[SiteCreate]]
