28906f7e8819e06c05fb7283506128749d3742a8
Tanner Hornsby <thornsby@uchicago.edu>
2023-07-27 14:00:25 -0500

    update saved_input test suite

--- tests/test_save.py
+++ tests/test_save.py
@@ -1,5 +1,6 @@
 import pytest
 import respx
+import json
 
 from starlette.config import environ
 from starlette.status import (
@@ -82,31 +83,30 @@ def test_update_saved_input(setup_database, client, data):
     if not full_res['results'][0]['value'] == data['data'][0]['value']:
         errors.append(f"test_get_last_saved_input failed to retrieved last saved input: {data['data'][0]['value']}")
 
-# test get all saved inputs for a user
 @pytest.mark.parametrize(
-    "data", [
-        { 'data': [
-            {'id': 1, 'value': 'test response 1'},
-            {'id': 2, 'value': 'test response 2'},
-            {'id': 3, 'value': 'test response 3'}
-            ], 'id': 1 }
+    "data", [ 
+        { 'data': [ 
+            {'id': 1, 'value': 'UPDATED: test response 1'},
+            {'id': 2, 'value': 'UPDATED: test response 2'},
+            {'id': 3, 'value': 'UPDATED: test response 3'} 
+          ], 'id': 1 }
     ]
 )
+
 def test_get_all_saved_input(setup_database, client, data):
     """
     Test that the /user-input endpoint returns a 200 and all saved inputs for a user
     """
     errors = []
     fake_jwt = "1.2.3"
-    resp = client.post(
-        "/user-input", json=data, headers={"Authorization": f"bearer {fake_jwt}"}
-    )
     resp = client.get("/user-input/all", headers={"Authorization": f"bearer {fake_jwt}"})
     full_res = resp.json()
 
     if not resp.status_code == 200:
-        errors.append(f"test_get_last_saved_input failed response code: {resp.status_code}")
-    if not full_res['results'][0]['value'] == data['data'][0]['value']:
-        errors.append(f"test_get_last_saved_input failed to retrieved last saved input: {data['data'][0]['value']}")
+        errors.append(f"test_get_all_saved_input failed response code: {resp.status_code}")
+    
+    # check that the response contains the expected data (all of the saved inputs for the user)
+    for saved_input in full_res['results']:
+        if not saved_input in data['data']:
+            errors.append(f"test_get_all_saved_input failed to retrieved all saved inputs for user: {data['data']}")
 
-    assert not errors, "errors occurred: \n{}".format("\n".join(errors))
