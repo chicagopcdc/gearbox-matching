532e02b821d40d38773a31360818e8403a5e500e
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2023-10-25 11:42:07 -0500

    Group schema validation working.

--- src/gearbox/schemas/match_form.py
+++ src/gearbox/schemas/match_form.py
@@ -1,7 +1,47 @@
 from xml.dom.minidom import Identified
-from pydantic import BaseModel, Extra, Json
+from pydantic import BaseModel, Extra, Json, validator
 from datetime import datetime
 from typing import Optional, Sequence, List, Union, Dict, Any
+
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from jsonschema.exceptions import SchemaError 
+
+match_form_group_schema = {
+    "type":"array",
+    "properties": {
+            "id": {"type": "number"},
+            "name": {"type": "string"}
+        },
+    "required": ["id","name"]
+}
+
+#        "fields": {"type:":"array",
+#            "id": {"type": "number"},
+#            "groupId": {"type": "number"},
+#            "name": {"type": "string"},
+#            "label": {"type": "string"},
+#            "type": {"type": "string"},
+#            "options": {"type:":"array",
+#                "value": {"type": "number"},
+#                "label": {"type": "string"},
+#                "description": {"type": "string"}
+#            },
+#            "showIf": {"type:":"array",
+#                "operator": {"type":"string", "enum":["AND","OR"] },
+#                "criteria": {"type":"array",
+#                "items": {
+#                    "anyOf": [
+#                        {"type":"number"},
+#                        {"$ref":"#"}
+#                    ]       
+#                }          
+#                }       
+#            }
+#        }
+#    },       
+#    "required": ["groups","fields"]
+#}       
 
 class MatchFormGroup(BaseModel):
     id: int
@@ -28,6 +68,22 @@ class MatchFormField(BaseModel):
 class MatchFormBase(BaseModel):
     groups: List[MatchFormGroup]
     fields: List[MatchFormField]
+
+    @validator('groups')
+    def check_valid_vs_groups_schema(cls, v):
+        
+        try:
+            # The validate method will throw a jsonschema.exceptions.ValidationError
+            # if algorithm_logic field fails to validate against the algorithm_logic_schema
+            validate(v, match_form_group_schema)
+        except ValidationError as e:
+            # ValueError will be caught and handled by the app (in main.py)
+            raise ValueError(f"ERROR VALIDATING match form groups: {e}")
+        except Exception as e:
+            # ValueError will be caught and handled by the app (in main.py)
+            raise ValueError(f"ERROR VALIDATING match form groups: {e}")
+
+        return v    
 
 class MatchForm(MatchFormBase):
     pass
