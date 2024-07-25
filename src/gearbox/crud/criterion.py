441527444855f458ed85d145a8cd4854381669a9
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2024-07-25 12:01:12 -0500

    Fixed some crud and models.

--- src/gearbox/crud/criterion.py
+++ src/gearbox/crud/criterion.py
@@ -1,10 +1,10 @@
 from .base import CRUDBase
 from gearbox.models import Criterion
-from gearbox.schemas import CriterionCreate, CriterionSearchResults
+from gearbox.schemas import CriterionCreate, Criterion as CriterionSchema
 from sqlalchemy.orm import Session, joinedload
 from sqlalchemy import func, update, select, exc
 
-class CRUDCriterion(CRUDBase [Criterion, CriterionCreate, CriterionSearchResults]):
+class CRUDCriterion(CRUDBase [Criterion, CriterionCreate, CriterionSchema]):
 
     async def get_criterion_id_by_code(self, db: Session, code: str) -> int:
         stmt = select(Criterion.id).where(Criterion.code == code)
