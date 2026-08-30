d9c9acaaa050cb0e6891cfffc122e133d2976a37
stevekrasinsky <stevekrasinsky@users.noreply.github.com>
2026-08-30 17:34:34 -0500

    Mods to build.py.

--- build.py
+++ build.py
@@ -3,47 +3,43 @@ import shutil
 import subprocess
 import tempfile
 import re
-import shlex
 from pathlib import Path
+
+# This script is a poetry build directive which copies the alembic migrations from the branch or rev indicated in pyproject.toml.
 
 def build(setup_kwargs):
 	destination = Path(os.getcwd()) / "migrations"
 	with open("pyproject.toml","r",encoding="utf-8") as file:
 		for line in file:
 			if line.startswith("gearboxdatamodel"): 
-
-				match = re.search("(?P<url>https?://[^\s]+)\",\s*branch\s*=\s*\"(?P<branch>.*)\"",line)
+				match = re.search("(?P<url>https?://[^\s]+)\",\s*(branch|rev)\s*=\s*\"(?P<branch>.*)\"",line)
 				if match is not None:
 					repo_url=(match.group("url"))
 					branch=(match.group("branch"))
-					git_args = ["git", "clone", "-b", branch, repo_url],
+					git_args = ["git", "clone", "-b", branch, repo_url] 
+					with tempfile.TemporaryDirectory() as tmpdir:
+						git_args.append(tmpdir)
+						try: 
+							subprocess.run(
+								git_args, 
+								check=True, 
+								capture_output=True
+							)
+						except subprocess.CalledProcessError as e:
+							print(f"ERROR: {e.stderr}")
+							raise subprocess.CalledProcessError(f"Build.py build directive error in subprocess: {e}")
+						except Exception as e: 
+							raise Exception(f"Build.py build directive error in subprocess: {e}")
+
+						source_dir = Path(tmpdir) / "migrations"
+						if not source_dir.exists(): 
+							raise FileNotFoundError(f"{source_dir.name} not found in repo.")
+						if destination.exists():
+							shutil.rmtree(destination)
+						shutil.copytree(source_dir, destination)
+						print(f"Successfully copied {source_dir.name} to {destination}")
 				else:
-					match = re.search("(?P<url>https?://[^\s]+)\",\s*rev\s*=\s*\"(?P<rev>.*)\"",line)
-					repo_url=(match.group("url"))
-					rev=(match.group("rev"))
-					git_args = ["git", "clone", "-b", rev, repo_url]
-
-				with tempfile.TemporaryDirectory() as tmpdir:
-					git_args.append(tmpdir)
-					try:
-						subprocess.run(
-							git_args,
-                            check=True,
-                            capture_output=True
-						)
-					except subprocess.CalledProcessError as e:
-						print(f"ERROR: {e.stderr}")
-					except Exception as e:
-						print(f"{e}")
-
-					source_dir = Path(tmpdir) / "migrations"
-					if not source_dir.exists():
-						raise FileNotFoundError(f"{source_dir.name} not found in repo.") 
-
-					if destination.exists():
-						shutil.rmtree(destination)
-					shutil.copytree(source_dir, destination) 
-					print(f"Successfully copied {source_dir.name} to {destination}")
+					raise Exception("Unable to locate gearboxDataModel dependency in pyproject.toml.")
 
 if __name__ == "__main__":
 	build({})
